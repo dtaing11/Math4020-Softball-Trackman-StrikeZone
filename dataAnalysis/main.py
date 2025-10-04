27e88e83f79c51de412882af57fbe82dee0a188e
Dina Taing <dina@DinaTaing.attlocal.net>
2025-10-03 20:30:22 -0500

    Adding Virtual Enviroment

--- dataAnalysis/main.py
+++ dataAnalysis/main.py
@@ -1,18 +1,16 @@
-import csv
-import pandas as pd 
+import pandas as pd
 
-def extract_2024_data(input_csv_path, output_csv_path):
+def extract_2020_to_2024_data(input_csv_path):
     print(f"Reading: {input_csv_path}")
     df = pd.read_csv(input_csv_path)
-
     df['game_date'] = pd.to_datetime(df['game_date'], errors='coerce')
 
-    df_2024 = df[df['game_date'].dt.year == 2024]
+    for year in range(2020, 2025):  
+        df_year = df[df['game_date'].dt.year == year]
+        output_file = f"../datasets/pitches_{year}.csv"
+        print(f"Writing {len(df_year)} rows to: {output_file}")
+        df_year.to_csv(output_file, index=False)
 
-    print(f"Writing {len(df_2024)} rows to: {output_csv_path}")
-    df_2024.to_csv(output_csv_path, index=False)
-
-if __name__ == "__main__": 
-    input_file = "../datasets/mlb_pitch_data_2020_2024.csv"       
-    output_file = "../datasets/pitches_2024.csv"
-    extract_2024_data(input_file, output_file)
+if __name__ == "__main__":
+    input_file = "../datasets/mlb_pitch_data_2020_2024.csv"
+    extract_2020_to_2024_data(input_file)
